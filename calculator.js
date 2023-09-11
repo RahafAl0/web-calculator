801fa54a205fcef62af33130db5d206c46bf6d71
RahafAl0 <ra.al9760@gmail.com>
2023-09-11 15:09:38 +0300

    Make math operations work

--- calculator.js
+++ calculator.js
@@ -1,8 +1,10 @@
 let buffer = "0";
+let runningTotal = 0;
+let previousOperator;
 const screen = document.querySelector('.screen');
 
-function buttonClick(value){
-    if(isNaN(parseInt(value))) {
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
         handleSymbol(value);
     } else {
         handleNumber(value);
@@ -10,29 +12,86 @@ function buttonClick(value){
     rerender();
 }
 
-function handleNumber(number) {
+function handleNumber(value) {
     if (buffer === '0') {
-        buffer = number;
+        buffer = value;
     } else {
-        buffer += number;
+        buffer += value;
     }
-};
-
-function handleSymbol(symbol) {
-    console.log('symbol')
 }
 
-function init() {
-    console.log('hii')
-    document
-    .querySelector('.calc-buttons')
-    .addEventListener("click", function(event) {
-        buttonClick(event.target.innerText)
-    });
+function handleMath(value) {
+    if (buffer === '0') {
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+    previousOperator = value;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '−') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer;
+    } else if (previousOperator === '÷') {
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleSymbol(value) {
+        switch (value) {
+            case 'C':
+                buffer = '0';
+                runningTotal = 0;
+                break;
+            case '=':
+                if (previousOperator === null) {
+                    return;
+                }
+                flushOperation(parseInt(buffer));
+                previousOperator = null;
+                buffer ="" + runningTotal;
+                runningTotal = 0;
+                break;
+
+            case '←':
+                if (buffer.length === 1) {
+                    buffer = '0';
+                } else {
+                    buffer = buffer.substring(0, buffer.length - 1);
+                }
+                break;
+            case '+':
+            case '−':
+            case '×':
+            case '÷':
+                handleMath(value);
+                break;
+        }
 }
 
 function rerender() {
     screen.innerText = buffer;
 }
+
+function init() {
+    console.log('hii')
+    document
+        .querySelector('.calc-buttons')
+        .addEventListener("click", function (event) {
+            buttonClick(event.target.innerText)
+        });
+}
+
+
 
 init(); 
